11d36f0a9e64572c051cd8910cf4115ffa8abb9b
KaviduNew <pradipnewrasanga@gmail.com>
2024-12-12 19:45:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
+SESSION_ID: process.env.SESSION_ID || "TN0mwBaQ#GJruXP_VB8JZ0NT08Dhiv6v0IOewaJlCQoDx3eVG4XY", //put your session id
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/JCByNJ4/adb435400eafa402.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am nadeen poorna*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
 MODE: process.env.MODE || "public", //private or public
